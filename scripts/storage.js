bc637f8e2d29a7d9ee2b63e2b7be5ae8b955dcf5
Tim Guggenmos <tim.guggenmos@me.com>
2020-01-08 07:56:19 +0100

    storage implementation

---
+++ scripts/storage.js
@@ -0,0 +1,41 @@
+class Storage {
+    constructor(localStorage) {
+        this.localStorage = localStorage;
+    }
+    get color() {
+        if (!this._color) {
+            this._color = this.localStorage.getItem("color");
+        }
+        return this._color;
+    }
+    set color(value) {
+        this._color = value;
+        this.localStorage.setItem("color", value);
+    }
+}
+
+function addGetSet(obj, attr) {
+    Object.defineProperties(obj, {
+        [attr]: {
+            get: function () {
+                if (!this["_" + attr]) {
+                    this["_" + attr] = this.localStorage.getItem(attr);
+                }
+                return this["_" + attr];
+            },
+            set: function (value) {
+                if (typeof value !== "string")
+                    console.error(`Storage.${attr} = ${value} is not of type string.`)
+                this["_" + attr] = value;
+                this.localStorage.setItem(attr, value);
+            }
+        }
+    })
+}
+
+addGetSet(Storage.prototype, "name");
+addGetSet(Storage.prototype, "comment");
+addGetSet(Storage.prototype, "color");
+addGetSet(Storage.prototype, "vibration");
+
+let storage = new Storage(localStorage);
