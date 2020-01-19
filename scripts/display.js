9db3f25e4859e09187f4b0593a2eb888dfde8107
missing-user <philipp.jurasic@gmx.de>
2020-01-19 13:13:42 +0100

    message changed

--- scripts/display.js
+++ scripts/display.js
@@ -28,7 +28,7 @@ const possibleButtonLabels = [
 
 /** The message displayed by "help" command. */
 const HELP_MESSAGE = [
-  "Type  / followed by the command into the comment box",
+  "Syntax:  /{command}",
   "Click colors:",
   "    " + ["green", "purple", "blue", "yellow", "black", "white"].join(", "),
   "Click lines:",
