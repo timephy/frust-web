cb1aedb31015d53aa1f373b8af54b8c0199ec292
Tim Guggenmos <tim.guggenmos@me.com>
2020-01-29 22:41:19 +0100

    refactor cd setup scripts

--- cd_setup_cache.py
+++ cd_setup_cache.py
@@ -5,8 +5,6 @@ import json
 files = list(sys.stdin)
 files = [f"/{f[:-1]}" for f in files]
 
-print(os.environ)
-
 sw_content = open("sw.js", "r").read()
 sw_content = sw_content.replace("{{COMMIT_SHA}}", os.getenv("GITHUB_SHA"))
 sw_content = sw_content.replace("""["{{CACHE_URLS}}"]""",
--- get-version.js
+++
@@ -1,9 +0,0 @@
-const commit_sha = process.env["GITHUB_SHA"];
-const timestamp = new Date().toISOString();
-
-const output = {
-    commit_sha,
-    timestamp
-};
-
-console.log(JSON.stringify(output, null, 4));
