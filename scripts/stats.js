5cf7389cf2dc729abadb24da937f376e655438be
missing-user <philipp.jurasic@gmx.de>
2020-01-29 23:09:54 +0100

    charts

--- scripts/stats.js
+++ scripts/stats.js
@@ -63,6 +63,53 @@ window.addEventListener('load', () => {
   loadJson((error, result) => {
     if (error)
       console.log(error);
+
+    console.log("raw result ", result)
+    result = groupBy(result, event => event.name)
+    console.log("grouped result ", result)
+    result = Object.entries(result).map((key) => {
+      // console.log(key);
+      const events = key[1];
+      const firstEvent = events[0];
+      return {
+        count: events.length,
+        event: firstEvent.name
+      };
+    });
+    console.log("final result ", result)
+    var values = [];
+    var names = [];
+    values = result.map(x => x.count)
+    names = result.map(x => x.event)
+
+    makeChart({
+      names: names,
+      values: values
+    }, 'eventChart', 'Events (chart)');
+  }, '/api/latest_events');
+
+  loadJson((error, result) => {
+    if (error)
+      console.log(error);
+
+    result = result.sort((a, b) => a.timestamp - b.timestamp);
+
+    var timestamps = [];
+    var clicks = [];
+    var events = [];
+    timestamps = result.map(x => x.timestamp)
+    clicks = result.map(x => x.click_count)
+    events = result.map(x => x.event_count)
+    makeLineChart({
+      timestamps: timestamps,
+      events: events,
+      clicks: clicks.
+    });
+  }, '/api/latest_hours');
+
+  loadJson((error, result) => {
+    if (error)
+      console.log(error);
     CreateTextFromJSON(result, 'versionData', 'Frontend version:');
   }, '/version.json');
 
@@ -71,35 +118,35 @@ window.addEventListener('load', () => {
       console.log(error);
     CreateTextFromJSON(result, 'api-version', 'Backend version:');
   }, '/api');
-  makeChart();
-  makeLineChart();
 });
 
-function makeChart() {
-  var ctx = document.getElementById('eventChart');
+function makeChart(res, eid, caption) {
+  var ctx = document.getElementById(eid);
+
+  var cap = ctx.createCaption();
+  cap.textContent = caption;
+
+  var colors = [];
+  var bcolors = [];
+  for (var i = 0; i < res.values.length; i++) {
+    colors.push(i % 2 == 0 ?
+      'rgba(183, 28, 28, 1)' :
+      'rgba(127, 0, 0, 1)')
+    bcolors.push(i % 2 == 1 ?
+      'rgba(183, 28, 28, 1)' :
+      'rgba(127, 0, 0, 1)')
+  }
+
+
   var myChart = new Chart(ctx, {
     type: 'bar',
     data: {
-      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      labels: res.names,
       datasets: [{
-        label: 'Events',
-        data: [12, 19, 3, 5, 2, 3],
-        backgroundColor: [
-          'rgba(183, 28, 28, 1)',
-          'rgba(127, 0, 0, 1)',
-          'rgba(183, 28, 28, 1)',
-          'rgba(127, 0, 0, 1)',
-          'rgba(183, 28, 28, 1)',
-          'rgba(127, 0, 0, 1)',
-        ],
-        borderColor: [
-          'rgba(127, 0, 0, 1)',
-          'rgba(183, 28, 28, 1)',
-          'rgba(127, 0, 0, 1)',
-          'rgba(183, 28, 28, 1)',
-          'rgba(127, 0, 0, 1)',
-          'rgba(183, 28, 28, 1)',
-        ],
+        label: caption,
+        data: res.values,
+        backgroundColor: colors,
+        borderColor: bcolors,
         borderWidth: 2
       }]
     },
@@ -115,34 +162,50 @@ function makeChart() {
   });
 }
 
-function makeLineChart() {
-  var ctx = document.getElementById('hourChart');
+function makeLineChart(res, eid, caption) {
+  var ctx = document.getElementById(eid);
   var myLineChart = new Chart(ctx, {
-      type: 'line',
-      data: {
-        datasets: [{
-          label: 'hourly clicks',
-          data: [12, 19, 3, 5, 2, 3],
-          backgroundColor: [
-            'rgba(183, 28, 28, 0.2)',
-            'rgba(127, 0, 0, 0.2)',
-            'rgba(183, 28, 28, 0.2)',
-            'rgba(127, 0, 0, 0.2)',
-            'rgba(183, 28, 28, 0.2)',
-            'rgba(127, 0, 0, 0.2)',
-          ],
-          borderColor: [
-            'rgba(127, 0, 0, 1)',
-            'rgba(183, 28, 28, 1)',
-            'rgba(127, 0, 0, 1)',
-            'rgba(183, 28, 28, 1)',
-            'rgba(127, 0, 0, 1)',
-            'rgba(183, 28, 28, 1)',
-          ],
-          borderWidth: 2
-        }]
+    type: 'line',
+    data: {
+      datasets: [{
+        labels: res.timestamps,
+        backgroundColor: 'rgba(127, 0, 0, 1)',
+        borderColor: 'rgba(183, 28, 28, 1)',
+        data: res.values,
+        fill: false,
+      }]
+    },
+    options: {
+      responsive: true,
+      title: {
+        display: true,
+        text: 'Chart.js Line Chart'
       },
-    options: {}
+      tooltips: {
+        mode: 'index',
+        intersect: false,
+      },
+      hover: {
+        mode: 'nearest',
+        intersect: true
+      },
+      scales: {
+        xAxes: [{
+          display: true,
+          scaleLabel: {
+            display: true,
+            labelString: 'Hours'
+          }
+        }],
+        yAxes: [{
+          display: true,
+          scaleLabel: {
+            display: true,
+            labelString: 'Clicks'
+          }
+        }]
+      }
+    }
   });
 }
 
