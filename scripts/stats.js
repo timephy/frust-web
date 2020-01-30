f296345e4c1573da63c1777bee2c1e77c3d9fc6b
Tim Guggenmos <tim.guggenmos@me.com>
2020-01-30 16:23:35 +0100

    reordered loading sequence (load clicks last)

--- scripts/stats.js
+++ scripts/stats.js
@@ -29,57 +29,6 @@ window.addEventListener('load', () => {
   loadJson((error, result) => {
     if (error) console.log(error);
 
-    result = groupBy(result, click => click.user + "\n" + click.comment)
-    result = Object.entries(result).map((key) => {
-      // console.log(key);
-      const clicks = key[1];
-      const firstClick = clicks[0];
-      return {
-        count: clicks.length,
-        user: firstClick.user,
-        comment: firstClick.comment
-      };
-    });
-    result = result.sort((a, b) => b.count - a.count);
-
-    // CreateTableFromJSON(result, 'latest-clicks', 'Klicks (heute)');
-    createTable(result, "latest-clicks", "Klicks (heute)", {
-      "Nutzer": "user",
-      "Anzahl": "count",
-      "Kommentar": "comment"
-    });
-  }, '/api/latest_clicks');
-
-  loadJson((error, result) => {
-    if (error) console.log(error);
-
-    console.log("raw result ", result)
-    result = groupBy(result, event => event.user + "\n" + event.name)
-    console.log("grouped result ", result)
-    result = Object.entries(result).map((key) => {
-      // console.log(key);
-      const events = key[1];
-      const firstEvent = events[0];
-      return {
-        count: events.length,
-        user: firstEvent.user,
-        event: firstEvent.name
-      };
-    });
-    result = result.sort((a, b) => b.count - a.count);
-    // console.log("final result ", result)
-
-    // CreateTableFromJSON(result, 'latest-events', 'Events (heute)');
-    createTable(result, "latest-events", "Events (heute)", {
-      "Nutzer": "user",
-      "Anzahl": "count",
-      "Event": "event"
-    });
-  }, '/api/latest_events');
-
-  loadJson((error, result) => {
-    if (error) console.log(error);
-
     console.log("raw result ", result)
     result = groupBy(result, event => event.name)
     console.log("grouped result ", result)
@@ -127,4 +76,55 @@ window.addEventListener('load', () => {
     if (error) console.log(error);
     CreateTextFromJSON(result, 'api-version', 'Backend version:');
   }, '/api');
+
+  loadJson((error, result) => {
+    if (error) console.log(error);
+
+    console.log("raw result ", result)
+    result = groupBy(result, event => event.user + "\n" + event.name)
+    console.log("grouped result ", result)
+    result = Object.entries(result).map((key) => {
+      // console.log(key);
+      const events = key[1];
+      const firstEvent = events[0];
+      return {
+        count: events.length,
+        user: firstEvent.user,
+        event: firstEvent.name
+      };
+    });
+    result = result.sort((a, b) => b.count - a.count);
+    // console.log("final result ", result)
+
+    // CreateTableFromJSON(result, 'latest-events', 'Events (heute)');
+    createTable(result, "latest-events", "Events (heute)", {
+      "Nutzer": "user",
+      "Anzahl": "count",
+      "Event": "event"
+    });
+  }, '/api/latest_events');
+
+  loadJson((error, result) => {
+    if (error) console.log(error);
+
+    result = groupBy(result, click => click.user + "\n" + click.comment)
+    result = Object.entries(result).map((key) => {
+      // console.log(key);
+      const clicks = key[1];
+      const firstClick = clicks[0];
+      return {
+        count: clicks.length,
+        user: firstClick.user,
+        comment: firstClick.comment
+      };
+    });
+    result = result.sort((a, b) => b.count - a.count);
+
+    // CreateTableFromJSON(result, 'latest-clicks', 'Klicks (heute)');
+    createTable(result, "latest-clicks", "Klicks (heute)", {
+      "Nutzer": "user",
+      "Anzahl": "count",
+      "Kommentar": "comment"
+    });
+  }, '/api/latest_clicks');
 });
