54e74f779442ddcb2d7fa5bde3823127770e66d9
Tim Guggenmos <tim.guggenmos@me.com>
2020-02-05 18:21:40 +0100

    only show last 2*24 hours

--- scripts/stats.js
+++ scripts/stats.js
@@ -56,6 +56,8 @@ window.addEventListener('load', () => {
   loadJson((error, result) => {
     if (error) console.log(error);
 
+    result = result.slice(-24 * 2); // only show last 2 days of hours
+
     function labelForTimestamp(timestamp) {
       const date = new Date(timestamp * 1000);
       const daysOfWeek = ["Mo", "Di", "Mi", "Do", "Fr", "Sa", "So"]
