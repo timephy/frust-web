8adfd7ee80e6fff16f233ee06d1192d98356d1b1
Tim Guggenmos <tim.guggenmos@me.com>
2020-02-18 10:16:01 +0100

    code styling + refactor

--- scripts/stats.js
+++ scripts/stats.js
@@ -1,37 +1,37 @@
-window.addEventListener('load', () => {
+window.addEventListener("load", () => {
   loadJson((error, result) => {
     if (error) console.log(error);
 
     result = result.filter((el) => el.name != null);
 
-    // CreateUserTableFromJSON(result, 'showOnlineUsers', 'Nutzer (online)');
+    // CreateUserTableFromJSON(result, "showOnlineUsers", "Nutzer (online)");
     createTable(result, "showOnlineUsers", `online Nutzer (${result.length})`, {
       "Name": "name",
       "Klicks (Session)": "click_count_session",
       "Events (Session)": "event_count_session"
     });
-  }, '/api/online_users');
+  }, "/api/online_users");
 
   loadJson((error, result) => {
     if (error) console.log(error);
 
-    console.log(result)
+    console.log(result);
     result = result.sort((a, b) => b.click_count - a.click_count);
 
-    // CreateTableFromJSON(result, 'showUsers', 'Nutzer (alle)');
+    // CreateTableFromJSON(result, "showUsers", "Nutzer (alle)");
     createTable(result, "showUsers", `alle Nutzer (${result.length})`, {
       "Name": "name",
       "Klicks": "click_count",
       "Events": "event_count"
     });
-  }, '/api/users');
+  }, "/api/users");
 
   loadJson((error, result) => {
     if (error) console.log(error);
 
-    console.log("raw result ", result)
-    result = groupBy(result, event => event.name)
-    console.log("grouped result ", result)
+    console.log("raw result ", result);
+    result = groupBy(result, event => event.name);
+    console.log("grouped result ", result);
     result = Object.entries(result).map((key) => {
       // console.log(key);
       const events = key[1];
@@ -41,17 +41,17 @@ window.addEventListener('load', () => {
         event: firstEvent.name
       };
     });
-    console.log("final result ", result)
+    console.log("final result ", result);
     var values = [];
     var names = [];
-    values = result.map(x => x.count)
-    names = result.map(x => x.event)
+    values = result.map(x => x.count);
+    names = result.map(x => x.event);
 
     makeChart({
       names: names,
       values: values
-    }, 'eventChart', 'Events (heute)');
-  }, '/api/latest_events');
+    }, "eventChart", "Events (heute)");
+  }, "/api/latest_events");
 
   loadJson((error, result) => {
     if (error) console.log(error);
@@ -62,39 +62,39 @@ window.addEventListener('load', () => {
     function labelForTimestamp(timestamp) {
       const date = new Date(timestamp * 1000);
       // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/getDay
-      const daysOfWeek = ["So", "Mo", "Di", "Mi", "Do", "Fr", "Sa"]
+      const daysOfWeek = ["So", "Mo", "Di", "Mi", "Do", "Fr", "Sa"];
 
-      return daysOfWeek[date.getDay()] + " " + date.getHours() + ":00"
+      return daysOfWeek[date.getDay()] + " " + date.getHours() + ":00";
     }
 
     // Date takes time in ms not s, get local time
-    const timestamps = result.map(x => labelForTimestamp(x.timestamp))
+    const timestamps = result.map(x => labelForTimestamp(x.timestamp));
     //const timestamps = result.map(x => new Date(x.timestamp * 1000).getHours() + ":00")
-    const clicks = result.map(x => x.click_count)
-    const events = result.map(x => x.event_count)
+    const clicks = result.map(x => x.click_count);
+    const events = result.map(x => x.event_count);
     makeLineChart({
       timestamps: timestamps,
       events: events,
       clicks: clicks
-    }, 'hourChart', 'Clicks (heute)');
-  }, '/api/latest_hours');
+    }, "hourChart", "Clicks (heute)");
+  }, "/api/latest_hours");
 
   loadJson((error, result) => {
     if (error) console.log(error);
-    CreateTextFromJSON(result, 'versionData', 'Frontend version:');
-  }, '/version.json');
+    CreateTextFromJSON(result, "versionData", "Frontend version:");
+  }, "/version.json");
 
   loadJson((error, result) => {
     if (error) console.log(error);
-    CreateTextFromJSON(result, 'api-version', 'Backend version:');
-  }, '/api');
+    CreateTextFromJSON(result, "api-version", "Backend version:");
+  }, "/api");
 
   loadJson((error, result) => {
     if (error) console.log(error);
 
-    console.log("raw result ", result)
-    result = groupBy(result, event => event.user + "\n" + event.name)
-    console.log("grouped result ", result)
+    console.log("raw result ", result);
+    result = groupBy(result, event => event.user + "\n" + event.name);
+    console.log("grouped result ", result);
     result = Object.entries(result).map((key) => {
       // console.log(key);
       const events = key[1];
@@ -108,18 +108,18 @@ window.addEventListener('load', () => {
     result = result.sort((a, b) => b.count - a.count);
     // console.log("final result ", result)
 
-    // CreateTableFromJSON(result, 'latest-events', 'Events (heute)');
+    // CreateTableFromJSON(result, "latest-events", "Events (heute)");
     createTable(result, "latest-events", `heutige Events (${result.length})`, {
       "Nutzer": "user",
       "Anzahl": "count",
       "Event": "event"
     });
-  }, '/api/latest_events');
+  }, "/api/latest_events");
 
   loadJson((error, result) => {
     if (error) console.log(error);
 
-    result = groupBy(result, click => click.user + "\n" + click.comment)
+    result = groupBy(result, click => click.user + "\n" + click.comment);
     result = Object.entries(result).map((key) => {
       // console.log(key);
       const clicks = key[1];
@@ -132,11 +132,11 @@ window.addEventListener('load', () => {
     });
     result = result.sort((a, b) => b.count - a.count);
 
-    // CreateTableFromJSON(result, 'latest-clicks', 'Klicks (heute)');
+    // CreateTableFromJSON(result, "latest-clicks", "Klicks (heute)");
     createTable(result, "latest-clicks", "Klicks (heute)", {
       "Nutzer": "user",
       "Anzahl": "count",
       "Kommentar": "comment"
     });
-  }, '/api/latest_clicks');
+  }, "/api/latest_clicks");
 });
